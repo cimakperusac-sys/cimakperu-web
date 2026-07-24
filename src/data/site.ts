0109478f5a97d132e68cf9a71871923518fc1eb5
CoronadoBryan <smithelunico1234@gmail.com>
2026-07-24 11:17:50 -0500

    cambios actualizacion

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,4 +1,20 @@
 export const site = {
   name: 'CIMAK Perú',
-  whatsapp: '51999999999', // reemplazar con el número real
+  legalName: 'Cimak Perú SAC',
+  ruc: '20609125919',
+  phone: '(+51) 912397609',
+  phoneHref: 'tel:+51912397609',
+  email: 'info@cimak.com',
+  address: 'Jr. 252 Sodio Urb. Industrial, Los Olivos - Lima',
+  whatsapp: '51912397609',
+  social: {
+    facebook: 'https://www.facebook.com/',
+    instagram: 'https://www.instagram.com/',
+    tiktok: 'https://www.tiktok.com/',
+    whatsapp: 'https://wa.me/51912397609',
+  },
+  mapEmbed:
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3903.111250904358!2d-77.06744932405218!3d-11.966798740418731!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105d5dfaa94d6ff%3A0xd2dd8bcb0b698a2a!2sCIMAK!5e0!3m2!1ses-419!2sus!4v1784580687374!5m2!1ses-419!2sus',
 } as const;
+
+
